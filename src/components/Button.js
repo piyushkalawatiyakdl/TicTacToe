9aa3b1092d646a8a6800e1a6365e1ee99cfed6cc
unknown <you@example.com>
2023-03-26 10:46:15 +0530

    ticTacToe Complete

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Button({title}) {
+  return (
+    <div>
+
+        {/* <button className='button1'> </button> */}
+    </div>
+  )
+}
+
+export default Button;
